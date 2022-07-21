268bedba090e1d27fc073188ae186d209bbca806
Chris Sturgill <chris@thesturgills.com>
2022-07-21 15:14:52 -0500

    Remove deprecated test

--- etldata/data_test.go
+++ etldata/data_test.go
@@ -20,34 +20,6 @@ func ExampleNewJSON() {
 	// Output: {"A":1,"B":2}
 }
 
-func ExampleParseJSON() {
-	d := []byte(`{"A":1,"B":2}`)
-	t := testStruct{}
-	etldata.ParseJSON(d, &t)
-
-	fmt.Println(fmt.Sprintf("%+v", t))
-	// Output: {A:1 B:2}
-}
-
-func ExampleObjectsFromJSON() {
-	d := []byte(`[{"One":1},
-		      {"Two":2}]`)
-
-	objects, _ := etldata.ObjectsFromJSON(d)
-
-	fmt.Println(fmt.Sprintf("%+v", objects))
-	// Output: [map[One:1] map[Two:2]]
-}
-
-func ExampleObjectsFromJSONIsNull() {
-	d := []byte("null")
-
-	objects, _ := etldata.ObjectsFromJSON(d)
-
-	fmt.Println(fmt.Sprintf("%+v", objects))
-	// Output: []
-}
-
 func ExampleJSONFromHeaderAndRows() {
 	header := []string{"A", "B", "C"}
 	rows := [][]interface{}{
